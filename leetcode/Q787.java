531f6d33dbf6d60e37870243f4b84f99253f021f
WOOSERK <55542546+WOOSERK@users.noreply.github.com>
2023-04-13 01:20:17 +0900

    리트코드

--- leetcode/Q787.java
+++ leetcode/Q787.java
@@ -7,26 +7,34 @@ class Solution {
         for(int[] f : flights)
             list[f[0]].add(new int[]{f[1], f[2]});
 
-        int[] values = new int[101];
-        Arrays.fill(values, Integer.MAX_VALUE);
+        int[][] values = new int[101][k+2];
+        for(int i=0; i<=n; i++)
+           Arrays.fill(values[i], Integer.MAX_VALUE);
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(5001, (a, b) -> Integer.compare(a[1], b[1]));
-        values[src] = 0;
-        pq.add(new int[]{src, 0, -1});
+        values[src][0] = 0;
+        pq.add(new int[]{src, 0, 0});
         while(!pq.isEmpty())
         {
             int[] tmp = pq.poll();
+            int cur = tmp[0];
+            int value = tmp[1];
+            int cnt = tmp[2];
 
-            for(int[] next : list[tmp[0]])
+            for(int[] next : list[cur])
             {
-                if(values[next[0]] <= values[tmp[0]] + next[1] || tmp[2] + 1 > k)
+                if(cnt > k || values[next[0]][cnt+1] <= values[cur][cnt] + next[1])
                     continue;
 
-                values[next[0]] = values[tmp[0]] + next[1];
-                pq.add(new int[]{next[0], values[next[0]], tmp[2] + 1});
+                values[next[0]][cnt+1] = values[cur][cnt] + next[1];
+                pq.add(new int[]{next[0], values[next[0]][cnt+1], cnt + 1});
             }
         }
 
-        return values[dst] == Integer.MAX_VALUE ? -1 : values[dst];
+        int answer = Integer.MAX_VALUE;
+        for(int value : values[dst])
+            answer = Math.min(answer, value);
+
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }
